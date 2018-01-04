8f4232997fa8ce133719d8b4f1bca0faadef1504
Anna Anks Nowak <anna.nowak@netguru.pl>
2018-01-04 01:02:21 +0100

    Let's try with doctest

--- 0021/02_droga.py
+++ 0021/02_droga.py
@@ -1,6 +1,13 @@
 def droga(t, a, vs=0):
+    ''' Napisz funkcję, która policzy drogę pokonaną przez samochód w czasie t i przyspieszeniu a z opcjonalną prędkością początkową, domyślnie równą 0.
+    >>> droga(5, 5)
+    62.5
+    >>> droga(10, 10, vs=100)
+    1500.0 '''
     result = vs*t + (a * (t **2))/2
-    print(result)
+    return result
 
-droga(5, 5)
-droga(10, 10, vs=100)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
